ca5cca3c468283e238a00362685248383521b4cd
Vedant <vedantdumbre0706@gmail.com>
2025-05-23 16:52:37 +0530

    Screen One(frontend) Done

--- Donor database/Frontend/src/App.jsx
+++ Donor database/Frontend/src/App.jsx
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
-import './index.css'
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import VolunteerEvents from "./pages/VolunteerEvents";
+import ResearchPrograms from "./pages/ResearchPrograms";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
-  )
+    <Router>
+      <div className="bg-pink-100 min-h-screen">
+        <header className="bg-white h-28 flex flex-col justify-between items-center  shadow-md">
+          <h1 className="text-xl font-bold text-center w-full py-5">VPM TRUST</h1>
+          <div className="flex gap-8">
+            <button className="text-pink-600 font-semibold hover:underline">
+              <Link to="/">VOLUNTEERING EVENT</Link>
+            </button>
+            <button className="text-pink-600 font-semibold hover:underline">
+              <Link to="/research">DONATE FOR RESEARCH</Link>
+            </button>
+          </div>
+        </header>
+        <Routes>
+          <Route path="/" element={<VolunteerEvents />} />
+          <Route path="/research" element={<ResearchPrograms />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
